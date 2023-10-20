210764686f378743ab62d1cc3f2badb774e25911
Jakob Millen <66464271+JMillen@users.noreply.github.com>
2023-10-20 18:06:52 +1300

    Features: Location permission request added, Added display for user's location on map In this commit:  - Methods related to checking, getting, and verifying user's location was added  - Displaying user's last location along with a map marker was added

--- app/src/main/java/com/example/assignmentthree/MapsActivity.java
+++ app/src/main/java/com/example/assignmentthree/MapsActivity.java
@@ -1,9 +1,19 @@
 package com.example.assignmentthree;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,15 +21,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.assignmentthree.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
 
+    FusedLocationProviderClient fusedLocationProviderClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        checkPermissionGranted();
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -28,6 +43,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -35,9 +51,120 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        getLastKnownLocation();
+        //mMap.addMarker(new MarkerOptions().position(userLocation).title("User Location"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
     }
+
+
+    /**
+     * Method that checks if location permissions have been granted.
+     */
+    public void checkPermissionGranted() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            //Permission not granted, so asking for permission
+            askForPermission();
+        } else {
+            //Permission was granted
+            permissionGranted();
+        }
+    }
+
+    /**
+     * Method that launches a locationPermissionRequest
+     */
+    public void askForPermission() {
+        ActivityResultLauncher<String[]> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
+            boolean fineGranted = result.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false);
+            boolean coarseGranted = result.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false);
+
+            if (fineGranted) {
+                // fine permission granted
+                permissionGranted();
+            } else if (coarseGranted) {
+                // coarse permission granted
+                checkPermissionGranted();
+            } else {
+                // No permissions have been granted
+                permissionNotGranted();
+            }
+        });
+
+        // Passing request for permissions
+        activityResultLauncher.launch(new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        });
+    }
+
+
+    /**
+     * Method that uses toast to display if permission was granted
+     */
+    public void permissionGranted() {
+        Toast.makeText(this, "GRANTED", Toast.LENGTH_SHORT).show();
+        getLastKnownLocation();
+    }
+
+    /**
+     * Method that uses toast to display if permission was not granted
+     */
+    public void permissionNotGranted() {
+        Toast.makeText(this, "NOT GRANTED", Toast.LENGTH_SHORT).show();
+    }
+
+
+    /**
+     * Method that gets the users last location and adds a marker to the map
+     */
+    public void getLastKnownLocation() {
+        // Get fusedLocation provider
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // Forced permission check because android studio doesn't realise we've already checked
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(userLocation).title("User Location"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+                } else {
+                    Toast.makeText(MapsActivity.this, "Error getting location", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * Method that takes last location and returns Latitude and Longitude
+     * of users location
+     * @param location
+     */
+    public LatLng getLatLngFromLocation(Location location) {
+        if (location != null) {
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+            return new LatLng(lat, lon);
+        } else {
+            Toast.makeText(this, "Error getting location", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
 }
